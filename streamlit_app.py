62d4573c5f12f482bdd9e35b3f6a38dce6d944de
Jainam bhavsar <jainam121005@gmail.com>
2026-06-15 00:53:21 +0530

    Enhance fruit options data handling in Streamlit app
    Updated the data retrieval process to include 'SEARCH_ON' column and converted to pandas DataFrame for better handling in Streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import pandas as pd
 import requests
 import os
 import time
@@ -18,19 +19,29 @@ st.write('The name on your smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table(
+    "smoothies.public.fruit_options"
+).select(
+    col('FRUIT_NAME'),
+    col('SEARCH_ON')
+)
+
+pd_df = my_dataframe.to_pandas()
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
-'choose upto 5 ingredients:'
-,my_dataframe
+    'Choose up to 5 ingredients:',
+    pd_df['FRUIT_NAME'],
+    max_selections=5
 )
-
 if ingredients_list:
     ingredients_string = ''
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_choosen,'SEARCH_ON'].iloc[0]
+        st.write('The search value for ',fruit_choosen,' is ',search_on,'.')
+        st.subheader(fruit_choosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
